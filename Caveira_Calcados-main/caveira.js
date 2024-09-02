d95ef42fe462d82205f53f358397b2a09f7d860b
JOÃO GABRIEL FILOMENO WIESE <joao_wiese@estudante.sesisenai.org.br>
2024-09-02 12:48:10 -0300

    Projeto em construção

---
+++ Caveira_Calcados-main/caveira.js
@@ -0,0 +1,11 @@
+function Entrar(){
+    window.location.href = "/Pagina-principal/pagina_principal.html"
+}
+
+function Cadastrar(){
+    window.location.href = "/Cadastro/Cadastro.html"
+}
+
+function Cadastrar(){
+    window.location.href = "/"
+}
